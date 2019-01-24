d4e6a3561afb82e9268e9caea4ad41176f37bb7f
Naoya Ozawa <n.ozawa@cns.s.u-tokyo.ac.jp>
2019-01-24 19:26:50 +0900

    Bq plot

---
+++ flux.C
@@ -0,0 +1,56 @@
+// フィットをもう少しいい感じで出来ないか？
+
+void flux(){
+    TCanvas *c1 = new TCanvas();
+
+    const char* data = "flux.dat";
+
+    TGraphErrors *cyric = new TGraphErrors(data,"%lg %lg %lg");
+    cyric->SetTitle("CYRIC (0.25 p#muA)");
+    cyric->SetMarkerColor(6);
+    cyric->SetMarkerStyle(11);
+
+
+//    TF1 *cyric_fit = new TF1("cyric_fit","[0]*TMath::Exp(-[1]*x) + [2]/(x*x) + [3]");
+//    cyric_fit->SetParameters(30.0,1.0,-5.0,0.0);
+
+//    TF1 *cyric_fit = new TF1("cyric_fit","[0]/(x*x) + [1]",0.5,8.0);
+//    cyric_fit->SetLineColor(6);
+//    cyric_fit->SetParameters(10.0,0.0);
+//    cyric->Fit(cyric_fit,"M","",0.5,8.0);
+
+    // CYRIC data: 1.5 uA = 0.3 puA (18-O 5+ ions)
+    // CNS data: 3 puA = 18 uA (18-O 6+ ions)
+    double conversion = 3.0/0.3;
+
+    TGraphErrors *cns = new TGraphErrors();
+    for (int i =0; i < cyric->GetN(); ++i){
+        cns->SetPoint(i,cyric->GetX()[i],conversion*cyric->GetY()[i]);
+        cns->SetPointError(i,0.0,conversion*cyric->GetEY()[i]);
+    }
+    cns->SetTitle("CNS (3.0 p#muA)");
+    cns->SetMarkerColor(4);
+    cns->SetMarkerStyle(22);
+
+//    TF1 *cns_estimate = new TF1("cns_estimate","[0]/(x*x) + [1]",0.5,8.0);
+//    cns_estimate->SetLineColor(4);
+//    cns_estimate->SetParameters(conversion*cyric_fit->GetParameter(0),conversion*cyric_fit->GetParameter(1));
+
+    TMultiGraph *comp = new TMultiGraph();
+    comp->Add(cyric);
+    comp->Add(cns);
+    comp->SetTitle("Neutron Radiation; Distance (m); Radiation (Bq)");
+
+    comp->Draw("AP*");
+
+    comp->GetYaxis()->SetRange(0.0,300.0);
+
+    gPad->BuildLegend();
+
+//    cns_estimate->Draw("SAME");
+
+
+    c1->Update();
+    c1->Modified();
+    
+}
